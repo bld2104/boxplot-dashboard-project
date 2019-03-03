4fcc8f5459c106e5d90567128eef8662dae1ed78
Sasha H <sasha.h@speedandfunction.com>
2019-03-02 19:20:46 -0500

    Integrate remote script processing

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,11 @@ COPY . /app
 
 RUN curl https://raw.githubusercontent.com/apex/apex/master/install.sh | sh
 
+RUN cd functions/dashboard-update-date/ && pip install --upgrade --target packages -r requirements.txt
+
 ENV AWS_ACCESS_KEY_ID *******************
 ENV AWS_SECRET_ACCESS_KEY *******************
 ENV AWS_REGION us-east-1
-
-RUN echo 'SASHA echo RUN'
 
 CMD apex deploy dashboard-update-date
 
--- functions/dashboard-update-date/main.py
+++ functions/dashboard-update-date/main.py
@@ -2,10 +2,20 @@ import sys
 
 sys.path.append('packages')
 
+import requests
 import json
 
 def handler(event, context):
+  script_path = "https://s3.amazonaws.com/homerise-dev/development/uploads/project/photos/92/initial_data_retrieval_twitter.py"
+
+  response = requests.get(script_path, allow_redirects=True)
+
+  # if status_code == 200
+  exec(response.content)
+  # else
+    # print("error !!")
+
   return {
     "statusCode": 200,
-    "body": json.dumps("It is over!")
+    "body": json.dumps("It is over! Or Error")
   }
